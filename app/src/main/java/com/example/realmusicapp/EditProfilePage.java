b6516243181888ea20b532ccb4665da12cc56a89
altw12 <anjelo@cahinosayan.com>
2021-08-03 17:53:46 +0800

    Music App Project COMT
    3/8 - added more stsuff
    
    2/8 - songs are playing, added working seekbar and timers, added addToPlaylist page, added repeatSong function, shuffleSongs not working
    
    29/7 - updated song collection, songs can now play
    
    27/7 - added 5 songs, added songdata transfer
           songs are not playing
    
    26/7 - sorta fixed scrollview (10th song not shown)
           added onclicklisteners to search song buttons

--- app/src/main/java/com/example/realmusicapp/EditProfilePage.java
+++ app/src/main/java/com/example/realmusicapp/EditProfilePage.java
@@ -2,13 +2,30 @@ package com.example.realmusicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class EditProfilePage extends AppCompatActivity {
+
+    private ImageButton editProfileBackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile_page);
+
+        editProfileBackButton = (ImageButton) findViewById(R.id.editProfileBackButton);
+        editProfileBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProfilePageFromEdit();
+            }
+        });
+    }
+    public void openProfilePageFromEdit(){
+        Intent intent = new Intent(this, ProfilePage.class);
+        startActivity(intent);
     }
 }
